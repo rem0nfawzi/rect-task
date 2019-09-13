df3414fa9ff123671c9264342a60313bc141b404
remon <remonfawzi0@gmail.com>
2019-09-13 23:46:05 +0200

    Small modification

--- src/components/Home/Dog.js
+++ src/components/Home/Dog.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Dog = ({ dog }) => {
+const Dog = ({ dog, myref }) => {
   return (
     <Link to={`/dog/${dog.ID}`} className='dog list-item'>
-      <p>{dog.Name}</p>
+      <p ref={myref}>{dog.Name}</p>
       <p>{dog.Race}</p>
       <p>{dog.Color}</p>
       <p>{dog.Birthdate}</p>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, createRef } from 'react';
 import { DogsContext } from '../../contexts/DogsContext';
 import axios from 'axios';
 import Dog from './Dog';
-
 const Home = () => {
+  const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop); // General scroll to element function
+
+  const [myRefs, setRefs] = useState([]);
   const { dogs, setDogs } = useContext(DogsContext);
   useEffect(() => {
     const fetchData = async () => {
@@ -20,14 +22,46 @@ const Home = () => {
           config
         );
         setDogs(res.data.Animals);
+        var myRefs = [];
+        for (let i = 0; i < res.data.Animals.length; i++) {
+          myRefs.push(createRef());
+        }
+        setRefs(myRefs);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, [setDogs]);
+  const [search, setSearch] = useState('');
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    var x = 0;
+    if (dogs.length > 0) {
+      for (let i = 0; i < dogs.length; i++) {
+        if (dogs[i].Name.startsWith(search)) {
+          x = i;
+          break;
+        }
+      }
+      if (x !== 0) scrollToRef(myRefs[x]);
+    }
+  };
   return (
     <section id='dogs'>
+      <div className='search'>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='search'
+            value={search}
+            onChange={handleChange}
+            placeholder='Search Dogs'
+          />
+        </form>
+      </div>
       <div className='dog titles'>
         <p>Name</p>
         <p>Race</p>
@@ -35,9 +69,9 @@ const Home = () => {
         <p>BirthDate</p>
         <p>Is Dangerous</p>
       </div>
-      {dogs.map(dog => (
-        <Dog key={dog.ID} dog={dog} />
-      ))}
+      {dogs.map((dog, index) => {
+        return <Dog key={dog.ID} dog={dog} myref={myRefs[index]} />;
+      })}
     </section>
   );
 };
